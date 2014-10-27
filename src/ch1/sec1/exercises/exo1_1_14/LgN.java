04f05314b7ca2685bee76491795a3fa03b8fc020
fengjianyun <libojuve@gmail.com>
2014-10-28 00:13:36 +0100

    ch1/sec1/ex1_1_14: 创建类及方法

---
+++ src/ch1/sec1/exercises/exo1_1_14/LgN.java
@@ -0,0 +1,21 @@
+package ch1.sec1.exercises.exo1_1_14;
+
+/**
+ * 1.1.14 Write a static method lg() that takes an int value N as argument and returns
+ * the largest int not larger than the base-2 logarithm of N. Do not use Math.
+ * 
+ * @author libojuve@gmail.com
+ * 
+ */
+public class LgN {
+
+    /**
+     * 求不大于Log2N的最大整数
+     * 
+     * @param N
+     * @return 返回不大于log2N的最大整数
+     */
+    public static int lg(int N) {
+        return 0;
+    }
+}
